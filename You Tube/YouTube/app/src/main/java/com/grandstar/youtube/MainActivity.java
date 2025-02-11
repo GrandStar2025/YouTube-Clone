88c55ca8c43cea9b9e7c6a12c18ae55cbbc769aa
GrandStar2003 <162890116+GrandStar2025@users.noreply.github.com>
2025-02-11 16:29:09 +0530

    Add files via upload

---
+++ You Tube/YouTube/app/src/main/java/com/grandstar/youtube/MainActivity.java
@@ -0,0 +1,92 @@
+package com.grandstar.youtube;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ExpandableListAdapter;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.grandstar.youtube.fragment.ExploreFragment;
+import com.grandstar.youtube.fragment.HomeFragment;
+import com.grandstar.youtube.fragment.LibraryFragment;
+import com.grandstar.youtube.fragment.SubscriptionsFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        frameLayout = findViewById(R.id.frame_layout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //1 2 23 4
+                switch (item.getItemId()){
+
+                    case R.id.home:
+                        HomeFragment homeFragment = new HomeFragment();
+                        selectedFragment(homeFragment);
+                        break;
+
+                    case R.id.explore:
+                        ExploreFragment exploreFragment = new ExploreFragment();
+                        selectedFragment(exploreFragment);
+                        break;
+
+                    case R.id.publish:
+                        Toast.makeText(MainActivity.this, "Add A video", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.subscriptions:
+                        SubscriptionsFragment subscriptionsFragment = new SubscriptionsFragment();
+                        selectedFragment(subscriptionsFragment);
+                        break;
+
+                    case R.id.library:
+                        LibraryFragment libraryFragment = new LibraryFragment();
+                        selectedFragment(libraryFragment);
+                        break;
+
+
+                }
+                return false;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    private void selectedFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
